661fc0b96f855e7b8fcd88f4ed02ad0205339dac
kpcorda <kpcorda@gmail.com>
2015-03-11 23:55:02 -0500

    update

---
+++ Code/script1.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.6span').click(function() 
+	var linkText = $(this).text();
+
+    
+    $('.btn btn-default dropdown-toggle').html(linkText);
+
+    
+    return false;
+  
+});
+});
